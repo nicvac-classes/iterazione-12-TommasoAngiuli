8eeaa5935e00b6d7fa5aa6493b8fc6934ea68063
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-10-09 20:13:09 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main() 
 {
     int costomoto, risparmi, nuovirisparmi, nrisparmi;
-
     cout << "inserisci il prezzo della moto" << endl;
     cin >> costomoto;
     cout << " a quanto ammontano i tuoi risparmi?" << endl;
     cin >> risparmi;
     nrisparmi = 0;
-    while (risparmi < costomoto) {
+    while (risparmi < costomoto) 
+    {
         cout << "mancano " << costomoto - risparmi << " euro" << endl;
         cout << "inserisci i nuovi risparmi" << endl;
         cin >> nuovirisparmi;
