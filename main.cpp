214087b3b9b0e80a8538e74269a14528b8c3258a
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-10-09 20:07:25 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+    int costomoto, risparmi, nuovirisparmi, nrisparmi;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    cout << "inserisci il prezzo della moto" << endl;
+    cin >> costomoto;
+    cout << " a quanto ammontano i tuoi risparmi?" << endl;
+    cin >> risparmi;
+    nrisparmi = 0;
+    while (risparmi < costomoto) {
+        cout << "mancano " << costomoto - risparmi << " euro" << endl;
+        cout << "inserisci i nuovi risparmi" << endl;
+        cin >> nuovirisparmi;
+        risparmi = risparmi + nuovirisparmi;
+        cout << "hai " << risparmi << " euro" << endl;
+        nrisparmi = nrisparmi + 1;
+    }
+    cout << "hai effettuato risparmi " << nrisparmi << " volte" << endl;
+    cout << "hai accumulato " << risparmi << " euro" << endl;
+    cout << "ti rimangono " << risparmi - costomoto << " euro" << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
